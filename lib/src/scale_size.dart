cc7d1eb3d4a680115ce5e8fc42dc9cc8e53731be
Yanuar Arifin <halo@yanuar.id>
2023-02-23 00:16:32 +0700

    add defaultSize, scale and space size

---
+++ lib/src/scale_size.dart
@@ -0,0 +1,28 @@
+class ScaleSize {
+  /// Get the scale of 0.25
+  static const double xxxs = 0.25;
+
+  /// Get the scale of 0.5
+  static const double xxs = 0.5;
+
+  /// Get the scale of 1.0
+  static const double xs = 1.0;
+
+  /// Get the scale of 1.25
+  static const double s = 1.25;
+
+  /// Get the scale of 1.5
+  static const double m = 1.5;
+
+  /// Get the scale of 2.0
+  static const double l = 2.0;
+
+  /// Get the scale of 3.0
+  static const double xl = 3.0;
+
+  /// Get the scale of 4.0
+  static const double xxl = 4.0;
+
+  /// Get the scale of 8.0
+  static const double xxxl = 8.0;
+}
---
+++ lib/src/space_size.dart
@@ -0,0 +1,31 @@
+import 'base.dart';
+import 'scale_size.dart';
+
+class SpaceSize {
+  /// Get the size of 2.
+  static double get xxxs => defaultSize * ScaleSize.xxxs;
+
+  /// Get the size of 4.
+  static double get xxs => defaultSize * ScaleSize.xxs;
+
+  /// Get the size of 8.
+  static double get xs => defaultSize;
+
+  /// Get the size of 10.
+  static double get s => defaultSize * ScaleSize.s;
+
+  /// Get the size of 12.
+  static double get m => defaultSize * ScaleSize.m;
+
+  /// Get the size of 16.
+  static double get l => defaultSize * ScaleSize.l;
+
+  /// Get the size of 24.
+  static double get xl => defaultSize * ScaleSize.xl;
+
+  /// Get the size of 32.
+  static double get xxl => defaultSize * ScaleSize.xxl;
+
+  /// Get the size of 64.
+  static double get xxxl => defaultSize * ScaleSize.xxxl;
+}
